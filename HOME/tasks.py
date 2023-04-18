7c38268214398a0e0cc9f233dde98ae36e3fa5dd
SH-ke <SHke_dev.163.com>
2023-04-18 19:11:53 +0800

    update models, crontab, ScheduledTask pwsh, ui, catelog in ip_manager.sh

--- HOME/tasks.py
+++ HOME/tasks.py
@@ -4,7 +4,8 @@ from HOME.models import IPAddress
 def update_host_status():
     print("\n\n\n\n\nupdate task\n\n\n\n\n")
     five_minutes_ago = timezone.now() - timezone.timedelta(minutes=5)
-    ip_addresses = IPAddress.objects.filter(latest_update__lte=five_minutes_ago)
+    # 挑选 host_status=False 的用户，判断是否超过5min未更新
+    ip_addresses = IPAddress.objects.filter(latest_update__lte=five_minutes_ago, host_status=True)
     for ip_address in ip_addresses:
         ip_address.host_status = False
         ip_address.save()
